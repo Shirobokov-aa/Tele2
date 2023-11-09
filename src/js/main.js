a5eecff7b4e7573bb6df01b6fadec91201e31ca1
Shirobokov-aa <136621836+Shirobokov-aa@users.noreply.github.com>
2023-11-09 22:26:18 +0300

    about__desc

--- src/js/main.js
+++ src/js/main.js
@@ -9,10 +9,10 @@ document.addEventListener('DOMContentLoaded', () => {
   let swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
-    // autoplay: {
-    //   delay: 2500,
-    //   disableOnInteraction: false,
-    // },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
